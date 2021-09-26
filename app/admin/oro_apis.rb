c035a9c8bebeb2d89ceb9ef06c509a58d220b16a
Shahid Nawaz <shahidjutt72@yahoo.com>
2021-09-26 21:14:32 +0500

    Oro api implementation active admin

---
+++ app/admin/oro_apis.rb
@@ -0,0 +1,21 @@
+ActiveAdmin.register OroApi do
+
+  permit_params :client_id, :client_secret, :oro_key, :refresh_key, :merchant_id
+
+  #  ____ ___  ___  _
+  # | |_ / / \| |_)| |\/|
+  # |_|  \_\_/|_| \|_|  |
+
+  form title: ->(oro_api) { oro_api.persisted? ? "Editing Oro Api" : "New Oro Api" } do |f|
+    f.semantic_errors(*f.object.errors.keys)
+    f.inputs do
+      f.input :client_id
+      f.input :client_secret
+      f.input :oro_key
+      f.input :refresh_key
+      f.input :merchant_id
+      actions
+    end
+  end
+  
+end
