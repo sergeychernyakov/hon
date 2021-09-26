35219b085bb2d84a1e726d25bff7452be83fc138
Shahid Nawaz <shahidjutt72@yahoo.com>
2021-09-27 01:25:51 +0500

    Light API logger implementations, logs would be maintained for each type of syncs, linked to light api and oro api as well

---
+++ app/services/orodoro/logger.rb
@@ -0,0 +1,36 @@
+class Orodoro::Logger
+  
+  def initialize(log_params = {})
+    @log_params = log_params
+  end
+
+  def log!
+    @light_api_log = LightApiLog.new(@log_params)
+    @light_api_log.contents = contents
+    @light_api_log.save
+  end
+
+  private
+
+  def contents    
+    "#{klass_name} # #{object_number} was #{event_in_past} on Orodoro"
+  end
+
+  def object_number
+    @object_number ||= @log_params[:light_api_entity_id]
+  end
+
+  def klass_name
+    @klass_name ||= @log_params[:entity_type]
+  end
+
+  def event_in_past
+    case @log_params[:event]
+    when LightApiLog::CREATE_EVENT
+      'created'
+    when LightApiLog::DESTROY_EVENT
+      'destroyed'
+    end
+  end
+
+end
